a6eafe804f1d877d3594f63858fc7efe0ae20816
jannik-ebert <jannikebert1@gmail.com>
2023-12-29 21:58:31 +0100

    created location repository that gets locations by city name and returns a list of GeoResponse objects

---
+++ Server/Repositories/LocationsRepository.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using WeatherSuggest.Server.Models;
+using WeatherSuggestions.Server.Services;
+
+namespace WeatherSuggest.Server.Repositories
+{
+    public class LocationsRepository : ILocationsRepository
+    {
+        private readonly ApiService _apiService;
+        private string? _apiKey;
+
+        public LocationsRepository(ApiService apiService)
+        {
+            _apiService = apiService;
+            _apiKey = _apiService.GetApiKey();
+        }
+
+        public async Task<IEnumerable<GeoResponse>?> GetLocationsAsync(string cityName)
+        {
+            List<GeoResponse>? locations = new();
+
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetStringAsync($"http://api.openweathermap.org/geo/1.0/direct?q={cityName}&limit=5&appid={_apiKey}");
+
+                var jsonData = JArray.Parse(response);
+
+                locations = jsonData.Select(json => new GeoResponse((JObject)json)).ToList();
+            }
+
+            return locations;
+        }
+    }
+}
