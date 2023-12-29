9456ab750a5ec8569927aae83cd5ef08172aceaa
jannik-ebert <jannikebert1@gmail.com>
2023-12-29 22:01:10 +0100

    created automapper profile for GeoResponse class and dto

---
+++ Server/Profiles/LocationMapProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WeatherSuggest.Server.Models;
+using WeatherSuggest.Shared.DTOs;
+
+namespace WeatherSuggest.Server.Profiles
+{
+    public class LocationMapProfile : Profile
+    {
+        public LocationMapProfile()
+        {
+            this.CreateMap<GeoResponse, GeoResponseDto>();
+        }
+    }
+}
