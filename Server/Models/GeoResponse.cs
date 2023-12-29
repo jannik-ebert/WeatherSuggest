730793290c0e58826be4ef17c3e24720600b9f06
jannik-ebert <jannikebert1@gmail.com>
2023-12-29 01:14:22 +0100

    added GeoResponse data model class for json response serialization

---
+++ Server/Models/GeoResponse.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Globalization;
+
+namespace WeatherSuggest.Server.Models
+{
+    public class GeoResponse
+    {
+        public string? Name { get; }
+        public double Lat { get; }
+        public double Lon { get; }
+        public string? Country { get; }
+        public string? State { get; }
+
+        public GeoResponse(string jsonResponse)
+        {
+            var jsonData = JArray.Parse(jsonResponse);
+
+            if (jsonData.Count > 0)
+            {
+                var firstItem = jsonData[0];
+
+                Name = firstItem.SelectToken("name")?.ToString() ?? string.Empty;
+                Lat = double.TryParse(firstItem.SelectToken("lat")?.ToString(), NumberStyles.Float, CultureInfo.CurrentCulture, out var latResult) ? latResult : 0.0;
+                Lon = double.TryParse(firstItem.SelectToken("lon")?.ToString(), NumberStyles.Float, CultureInfo.CurrentCulture, out var lonResult) ? lonResult : 0.0;
+                Country = firstItem.SelectToken("country")?.ToString() ?? string.Empty;
+                State = firstItem.SelectToken("state")?.ToString() ?? string.Empty;
+            }
+        }
+    }
+}
