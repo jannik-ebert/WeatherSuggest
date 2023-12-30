24070532917e9d1022800f9c53ea55206ce2a2f1
jannik-ebert <jannikebert1@gmail.com>
2023-12-30 13:13:36 +0100

    added locations api controller that returns the locations from the repository as a list of GeoResponse dtos

---
+++ Server/Controllers/LocationsController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WeatherSuggest.Server.Repositories;
+using WeatherSuggest.Shared.DTOs;
+
+namespace WeatherSuggest.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly ILocationsRepository _locationsRepository;
+        private readonly IMapper _mapper;
+
+        public LocationsController(ILocationsRepository locationsRepository, IMapper mapper)
+        {
+            _locationsRepository = locationsRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{city}")]
+        public async Task<IEnumerable<GeoResponseDto>> GetLocations(string city)
+        {
+            var locations = await _locationsRepository.GetLocationsAsync(city);
+            var locationDtos = _mapper.Map<List<GeoResponseDto>>(locations);
+
+            return locationDtos;
+        }
+    }
+}
