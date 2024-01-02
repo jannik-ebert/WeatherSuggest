7d4099e9836c2befb264f7648d691d9a64063571
jannik-ebert <jannikebert1@gmail.com>
2024-01-02 19:22:51 +0100

    created weather repository interface

---
+++ Server/Repositories/IWeatherRepository.cs
@@ -0,0 +1,9 @@
+﻿using WeatherSuggest.Server.Models;
+
+namespace WeatherSuggest.Server.Repositories
+{
+    public interface IWeatherRepository
+    {
+        public Task<WeatherResponse> GetWeatherDataAsync(double lat, double lon);
+    }
+}
