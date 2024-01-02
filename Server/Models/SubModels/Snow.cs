0b3d2bc8e329ade15aab5d3924a93f7dc87e17cf
jannik-ebert <jannikebert1@gmail.com>
2024-01-02 06:04:31 +0100

    added submodel class Snow

---
+++ Server/Models/SubModels/Snow.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Globalization;
+
+namespace WeatherSuggest.Server.Models.SubModels
+{
+    public class Snow
+    {
+        public double H1 { get; }
+        public double H3 { get; }
+
+        public Snow(JToken? snowData)
+        {
+            if (snowData is null)
+                throw new ArgumentNullException(nameof(snowData));
+
+            H1 = double.TryParse(snowData.SelectToken("1h")?.ToString(), NumberStyles.Float, CultureInfo.CurrentCulture, out var h1Result) ? h1Result : 0.0;
+            H3 = double.TryParse(snowData.SelectToken("3h")?.ToString(), NumberStyles.Float, CultureInfo.CurrentCulture, out var h3Result) ? h3Result : 0.0;
+        }
+    }
+}
