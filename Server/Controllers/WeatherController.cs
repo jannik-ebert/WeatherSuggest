5fbd117217045be4a255518a8e20b815c2662b99
jannik-ebert <jannikebert1@gmail.com>
2024-01-03 02:08:34 +0100

    added weather api controller with get method

---
+++ Server/Controllers/WeatherController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WeatherSuggest.Server.Repositories;
+using WeatherSuggest.Shared.DTOs;
+
+namespace WeatherSuggest.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly IWeatherRepository _weatherRepository;
+        private readonly IMapper _mapper;
+
+        public WeatherController(IWeatherRepository weatherRepository, IMapper mapper)
+        {
+            _weatherRepository = weatherRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<WeatherResponseDto> GetWeatherData(double lat, double lon)
+        {
+            var weatherData = await _weatherRepository.GetWeatherDataAsync(lat, lon);
+            var weatherDataDtos = _mapper.Map<WeatherResponseDto>(weatherData);
+
+            return weatherDataDtos;
+        }
+    }
+}
