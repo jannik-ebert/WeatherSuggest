bc1b152734bcab793b5ba61fff66fdaaeea7d9d4
jannik-ebert <jannikebert1@gmail.com>
2024-01-06 21:15:33 +0100

    added configuration helper class to read api key in the tests project

---
+++ Tests/ConfigurationHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Tests
+{
+    public class ConfigurationHelper
+    {
+        private static IConfigurationRoot _configuration;
+
+        static ConfigurationHelper()
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public static string GetApiKey()
+        {
+            return _configuration["ApiSettings:ApiKey"];
+        }
+    }
+}
